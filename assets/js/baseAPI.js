9692df363eeaf4b8bdc0ffd94dca9181eaec7945
qingnian <3247657780@qq.com>
2023-01-28 21:30:00 +0800

    完成个人中心功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -14,9 +14,12 @@ $.ajaxPrefilter(function (options) {
 
     // 全局统一挂载complete回调函数
     options.complete = function (res) {
-        // 1.强制清空token
-        localStorage.removeItem('token')
-        // 2.强制跳转到登录页面
-        location.href = '/login.html'
+        // 在complete回调这个函数中可以使用res.responseJSON拿到服务器响应回来的数据
+        if (res.responseJSON.status == 1 && res.responseJSON.message === '身份认证失败') {
+            // 1.强制清空token
+            localStorage.removeItem('token')
+            // 2.强制跳转到登录页面
+            location.href = '/login.html'
+        }
     }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -39,13 +39,13 @@ function getUserInfo() {
         },
         // // 不论成功还是失败，最终都会调用complete这个函数
         // complete: function (res) {
-        //     // 在complete回调这个函数中可以使用res.responseJSON拿到服务器响应回来的数据
-        //     if (res.responseJSON.status == 1 && res.responseJSON.message === '身份认证失败') {
-        //         // 1.强制清空token
-        //         localStorage.removeItem('token')
-        //         // 2.强制跳转到登录页面
-        //         location.href = '/login.html'
-        //     }
+            // // 在complete回调这个函数中可以使用res.responseJSON拿到服务器响应回来的数据
+            // if (res.responseJSON.status == 1 && res.responseJSON.message === '身份认证失败') {
+            //     // 1.强制清空token
+            //     localStorage.removeItem('token')
+            //     // 2.强制跳转到登录页面
+            //     location.href = '/login.html'
+            // }
         // }
     })
 }
